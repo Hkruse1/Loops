87db8a25a325d7cf7ab52bfc37229f6c84a65835
Hkruse1 <59974533+Hkruse1@users.noreply.github.com>
2020-02-13 11:52:56 -0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char response = ' ';
+            do
+            {
+                double numOfMarks, average, sum = 0;
+
+
+                Console.WriteLine("How many marks are you entering?");
+                numOfMarks = int.Parse(Console.ReadLine());
+
+                for (int count = 1; count <= numOfMarks; count++)
+                {
+                    int mark;
+                    Console.WriteLine($"Please enter mark #{count}");
+                    mark = int.Parse(Console.ReadLine());
+                    sum += mark;
+                }
+                average = sum / numOfMarks;
+
+                Console.WriteLine($"The average is {average}");
+
+                Console.WriteLine("Restart?");
+                response = char.Parse(Console.ReadLine());
+            } while (response == 'y');
+
+        }   
+    }
+}
